c7d8f3b5fc59371430aee14c73d7d588e4f3e65d
xaakla <diego.rb0307@gmail.com>
2024-12-10 00:07:19 -0300

    initial commit

---
+++ src/test/java/com/app/projecth/ProjectHApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.projecth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectHApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
