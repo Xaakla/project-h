828199c1dbe44df79127e7f5903db57da3dd4c6d
xaakla <diego.rb0307@gmail.com>
2024-12-10 00:11:02 -0300

    creating Team Entity

---
+++ src/main/java/com/app/projecth/database/entities/Team.java
@@ -0,0 +1,77 @@
+package com.app.projecth.database.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class Team {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String acronym;
+
+    @NotBlank
+    private String imageId;
+
+    @NotBlank
+    private String color;
+
+    public Team() {
+    }
+
+    public Team(String name, String acronym, String imageId, String color) {
+        this.name = name;
+        this.acronym = acronym;
+        this.imageId = imageId;
+        this.color = color;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAcronym() {
+        return acronym;
+    }
+
+    public void setAcronym(String acronym) {
+        this.acronym = acronym;
+    }
+
+    public String getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(String imageId) {
+        this.imageId = imageId;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
